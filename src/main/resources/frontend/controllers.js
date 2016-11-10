c8a3896b97e4db70734e00759fd28b25a37f5351
sihaya <sihaya@atreidis.nl.eu.org>
2016-11-10 23:29:41 +0100

    remove maps library because it does not work

--- src/main/resources/frontend/controllers.js
+++ src/main/resources/frontend/controllers.js
@@ -1,20 +1,13 @@
-angular.module('traffic', [ 'uiGmapgoogle-maps', 'chart.js' ]).config([ 'uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
-	uiGmapGoogleMapApiProvider.configure({
-		key : 'AIzaSyAqoNdH1eTyWqThlz1NlmcqsBwofjKslFE',
-		v : '3', // defaults to latest 3.X anyhow
-		libraries : 'weather,geometry,visualization'
-	});
-} ]);
+angular.module('traffic', [ 'chart.js', 'ui.bootstrap' ]);
 
-angular.module('traffic').controller('TrafficController', [ '$scope', '$http', function($scope, $http) {
-	$scope.map = {
-		center : {
-			latitude : 51.91691,
-			longitude : 4.36735
-		},
-		zoom : 14
-	};
-	
+angular.module('traffic').controller('TrafficController', [ '$scope', '$http', '$window', function($scope, $http, $window) {
+	$window.initMap = new google.maps.Map(document.getElementById('map-container'), {
+	    center: {
+	    	lat: 51.91691, lng: 4.36735
+	    },
+	    zoom: 14
+	});	
+			
 	var loadMeasurementPoints = function(maps) {
 		$http.get("http://localhost:4567/measurementpoints", {
 			params: {
